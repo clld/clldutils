2c6c6072c4dc20b10618237c237184eb1c1861ea
xrotwang <xrotwang@googlemail.com>
2016-01-20 10:48:15 +0100

    bumbed version number

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if not PY3:
 
 setup(
     name='clldutils',
-    version="0.6.1",
+    version="0.7.0",
     description='Utilities for clld apps',
     long_description="",
     author='Robert Forkel',
