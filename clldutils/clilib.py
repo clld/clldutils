9d706312fd339f437ae08428d2928243604b99aa
xrotwang <xrotwang@googlemail.com>
2017-01-11 10:01:32 +0100

    closes #24

--- clldutils/clilib.py
+++ clldutils/clilib.py
@@ -10,17 +10,53 @@ class ParserError(Exception):
     pass
 
 
+# Global registry for commands.
+# Note: This registry is global so it can only be used for one ArgumentParser instance.
+# Otherwise, different ArgumentParsers will share the same sub-commands which will rarely
+# be intended.
+_COMMANDS = []
+
+
+class Command(object):
+    def __init__(self, func, name=None):
+        self.func = func
+        self.name = name
+
+    @property
+    def doc(self):
+        return self.func.__doc__
+
+    def __call__(self, args):
+        return self.func(args)
+
+
+def command(name=None):
+    def wrap(f):
+        _COMMANDS.append(Command(f, name=name))
+        return f
+    return wrap
+
+
+def _attr(obj, attr):
+    return getattr(obj, attr, getattr(obj, '__{0}__'.format(attr), None))
+
+
 class ArgumentParser(argparse.ArgumentParser):
     """
-    An command line argument parser supporting sub-commands in a simple way.
+    A command line argument parser supporting sub-commands in a simple way.
+
+    Sub-commands can be registered in one of two ways:
+    - Passing functions as positional arguments into `ArgumentParser.__init__`.
+    - Decorating functions with the `command` decorator.
     """
     def __init__(self, pkg_name, *commands, **kw):
+        commands = commands or _COMMANDS
         kw.setdefault(
             'description', "Main command line interface of the %s package." % pkg_name)
         kw.setdefault(
             'epilog', "Use '%(prog)s help <cmd>' to get help about individual commands.")
         argparse.ArgumentParser.__init__(self, **kw)
-        self.commands = OrderedDict([(cmd.__name__, cmd) for cmd in commands])
+        self.commands = OrderedDict([(_attr(cmd, 'name'), cmd) for cmd in commands])
         self.pkg_name = pkg_name
         self.add_argument("--verbosity", help="increase output verbosity")
         self.add_argument('command', help='|'.join(self.commands.keys()))
@@ -31,7 +67,7 @@ class ArgumentParser(argparse.ArgumentParser):
         if args.command == 'help' and len(args.args):
             # As help text for individual commands we simply re-use the docstrings of the
             # callables registered for the command:
-            print(self.commands[args.args[0]].__doc__)
+            print(_attr(self.commands[args.args[0]], 'doc'))
         else:
             if args.command not in self.commands:
                 print('invalid command')
@@ -41,7 +77,7 @@ class ArgumentParser(argparse.ArgumentParser):
                 self.commands[args.command](args)
             except ParserError as e:
                 print(e)
-                print(self.commands[args.command].__doc__)
+                print(_attr(self.commands[args.command], 'doc'))
                 return 64
             except Exception as e:
                 if catch_all:
