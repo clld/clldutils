ef5ecc0b26f22f989ddeb7b6d8af21d0a6852d8d
xrotwang <xrotwang@googlemail.com>
2017-01-16 16:22:54 +0100

    pep8 and one more text function

--- clldutils/tests/test_text.py
+++ clldutils/tests/test_text.py
@@ -1,19 +1,28 @@
 # coding: utf8
 from __future__ import unicode_literals, print_function, division
-from clldutils.text import *
+from clldutils import text
+
+
+def test_truncate_with_ellipsis():
+    assert text.truncate_with_ellipsis(' '.join(30 * ['a']), ellipsis='.').endswith('.')
+    assert text.truncate_with_ellipsis(
+        ' '.join(30 * ['a']), ellipsis='.', width=100).endswith('a')
+
 
 def test_strip_brackets():
-    strings = ['arm(hand)', '(hand)arm', 'a(hand)r(hand)m(hand)', 'arm⁽hand⁾']
+    strings = ['arm((h)an[d])', '(hand)arm', 'a(hand)r(hand)m(hand)', 'arm⁽hand⁾']
     for string in strings:
-        assert strip_brackets(string) == 'arm'
-    assert strip_brackets('arm<hand>', brackets={"<": ">"}) == 'arm'
+        assert text.strip_brackets(string) == 'arm'
+    assert text.strip_brackets('arm<hand>', brackets={"<": ">"}) == 'arm'
+
 
 def test_split_text():
-    assert split_text('arm/hand')[1] == 'hand'
-    assert split_text('arm,hand;foot')[2] == 'foot'
-    assert split_text('arm/,;hand')[0] == 'arm'
-    assert split_text('arm/,;hand')[1] == 'hand'
-    assert split_text('arm/')[0] == 'arm'
+    assert text.split_text('arm/hand')[1] == 'hand'
+    assert text.split_text('arm,hand;foot')[2] == 'foot'
+    assert text.split_text('arm/,;hand')[0] == 'arm'
+    assert text.split_text('arm/,;hand')[1] == 'hand'
+    assert text.split_text('arm/')[0] == 'arm'
+
 
 def test_strip_chars():
-    assert strip_chars('b', 'abcabc') == 'acac'
+    assert text.strip_chars('b', 'abcabc') == 'acac'
--- clldutils/text.py
+++ clldutils/text.py
@@ -1,17 +1,20 @@
 # coding: utf8
 from __future__ import unicode_literals, print_function, division
 import re
+import textwrap
+
 
 def strip_brackets(text, brackets=None):
     """Strip brackets and what is inside brackets from text.
-    
-    .. bite:: If the text contains only one opening bracket, the rest of the text
-      will be ignored. This is a feature, not a bug, as we want to avoid that
-      this function raises errors too easily.
+
+    .. note::
+        If the text contains only one opening bracket, the rest of the text
+        will be ignored. This is a feature, not a bug, as we want to avoid that
+        this function raises errors too easily.
     """
     brackets = brackets or {
-            "(": ")", "{": "}", "[": "]", "（": "）", "【": "】", "『": "』",
-            "«": "»", "⁽": "⁾", "₍": "₎"}
+        "(": ")", "{": "}", "[": "]", "（": "）", "【": "】", "『": "』",
+        "«": "»", "⁽": "⁾", "₍": "₎"}
     stack = []
     new_sequence = ''
     for itm in text:
@@ -23,10 +26,16 @@ def strip_brackets(text, brackets=None):
             stack.pop(stack.index(itm))
     return new_sequence
 
+
 def split_text(text, separators="/,;~"):
     """Split text along the separators."""
-    return [x for x in re.split(r'\s*['+separators+']+\s*', text) if x.strip()]
+    return [x for x in re.split(r'\s*[' + separators + ']+\s*', text) if x.strip()]
+
 
 def strip_chars(chars, sequence):
     """Strip the specified chars from anywhere in the text."""
     return ''.join([s for s in sequence if s not in chars])
+
+
+def truncate_with_ellipsis(t, ellipsis='\u2026', width=40, **kw):
+    return t if len(t) <= width else textwrap.wrap(t, width=width, **kw)[0] + ellipsis
