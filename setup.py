f64ad81589156c112c634f67edd69ccf56fc9d9f
xrotwang <xrotwang@googlemail.com>
2017-01-25 12:44:40 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(fname):
 
 setup(
     name='clldutils',
-    version="1.7.2",
+    version="1.7.3",
     description='Utilities for clld apps',
     long_description=read("README.rst"),
     author='Robert Forkel',
