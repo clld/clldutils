0db760f5e97bd855a5e8c6d0d8bc50a7e6f30198
xrotwang <xrotwang@googlemail.com>
2018-09-20 08:48:41 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clldutils',
-    version='2.5.1',
+    version='2.5.2.dev0',
     description='Utilities for clld apps',
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
