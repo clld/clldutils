bb141a46a6ccc7a63ba14d8a190c8e40c7f4db5b
xrotwang <xrotwang@googlemail.com>
2019-10-10 14:52:07 +0200

    closes #84

---
+++ src/clldutils/coordinates.py
@@ -0,0 +1,177 @@
+from math import floor
+import re
+
+__all__ = ['Coordinates', 'dec2degminsec', 'degminsec2dec', 'degminsec']
+
+DEGREES = "°"
+MINUTES = "\u2032"
+SECONDS = "\u2033"
+
+PATTERNS = {
+    'lat_alnum': re.compile("(?P<deg>\d+)d(?P<min>[0-9]+)?(?P<sec>'\d+'')?(?P<hem>S|N)"),
+    'lon_alnum': re.compile("(?P<deg>\d+)d(?P<min>\d+)?(?P<sec>'\d+'')?(?P<hem>E|W)"),
+    'lat_degminsec': re.compile(
+        '(?P<deg>\d+)\s*%s\s*((?P<min>\d+)\s*%s\s*)?((?P<sec>[\d.]+)\s*%s\s*)?(?P<hem>S|N)' % (
+            DEGREES, MINUTES, SECONDS)),
+    'lon_degminsec': re.compile(
+        '(?P<deg>\d+)\s*%s\s*((?P<min>\d+)\s*%s\s*)?((?P<sec>[\d.]+)\s*%s\s*)?(?P<hem>E|W)' % (
+            DEGREES, MINUTES, SECONDS)),
+}
+
+
+def degminsec(dec, hemispheres):
+    """
+    >>> degminsec(2.4, 'NS')
+    "2°24'N"
+    """
+    if 'N' in hemispheres:
+        return Coordinates(dec, 0).lat_to_string(format='ascii')
+    return Coordinates(0, dec).lon_to_string(format='ascii')
+
+
+def dec2degminsec(dec):
+    """
+    convert a floating point number of degrees to a triple
+    (int degrees, int minutes, float seconds)
+
+    >>> assert dec2degminsec(30.50) == (30, 30, 0.0)
+    """
+    degrees = int(floor(dec))
+    dec = (dec - int(floor(dec))) * 60
+    minutes = int(floor(dec))
+    dec = (dec - int(floor(dec))) * 60
+    seconds = dec
+    return degrees, minutes, seconds
+
+
+def degminsec2dec(degrees, minutes, seconds):
+    """
+    convert a triple (int degrees, int minutes, float seconds) to
+    a floating point number of degrees
+
+    >>> assert dec2degminsec(degminsec2dec(30,30,0.0)) == (30,30,0.0)
+    """
+    dec = float(degrees)
+    if minutes:
+        dec += float(minutes) / 60
+    if seconds:
+        dec += float(seconds) / 3600
+    return dec
+
+
+class Coordinates(object):
+    """
+    >>> c = Coordinates('13dN', 0)
+    >>> assert c.latitude >= 13
+    >>> assert c.latitude <= 13.1
+    >>> c = Coordinates(0, 0)
+    >>> assert c.lat_to_string() == '0dN'
+    >>> assert c.lon_to_string() == '0dE'
+    >>> c = Coordinates(12.17, 92.83)
+    >>> assert c.lat_to_string() == '12d10N'
+    >>> assert c.lon_to_string() == '92d49E'
+    >>> c = Coordinates(-12.17, -92.83)
+    >>> assert c.lat_to_string() == '12d10S'
+    >>> assert c.lon_to_string() == '92d49W'
+    >>> lat, lon = '12d30N', '60d30E'
+    >>> c = Coordinates(lat, lon)
+    >>> assert c.lat_to_string() == lat
+    >>> assert c.lon_to_string() == lon
+    """
+
+    def __init__(self, lat, lon, format='alnum'):
+        if isinstance(lat, float):
+            self.latitude = lat
+        elif isinstance(lat, int):
+            self.latitude = float(lat)
+        else:
+            self.latitude = self.lat_from_string(lat, format)
+
+        if isinstance(lon, float):
+            self.longitude = lon
+        elif isinstance(lon, int):
+            self.longitude = float(lon)
+        else:
+            self.longitude = self.lon_from_string(lon, format)
+
+    def _match(self, string, type, format):
+        if isinstance(string, bytes):
+            string = string.decode('utf8')
+
+        if type + '_' + format in PATTERNS:
+            p = PATTERNS[type + '_' + format]
+        else:
+            p = PATTERNS[type + '_alnum']
+
+        m = p.match(string)
+        if not m:
+            raise ValueError(string)
+        return m
+
+    def lat_from_string(self, lat, format='alnum'):
+        m = self._match(lat, 'lat', format)
+        dec = degminsec2dec(m.group('deg'), m.group('min'), m.group('sec'))
+        if m.group('hem') == 'S':
+            dec = -dec
+        return dec
+
+    def lon_from_string(self, lon, format='alnum'):
+        m = self._match(lon, 'lon', format)
+        dec = degminsec2dec(m.group('deg'), m.group('min'), m.group('sec'))
+        if m.group('hem') == 'W':
+            dec = -dec
+        return dec
+
+    def _format(self, degrees, minutes, seconds, hemisphere, format):
+        seconds = int(round(seconds))
+        if seconds == 60:
+            minutes += 1
+            seconds = 0
+
+        if 120 > minutes >= 60:  # pragma: no cover
+            # This case cannot really happen, because we only ever feed the results of
+            # dec2degminsec into this method.
+            degrees += 1
+            minutes -= 60
+
+        if format == 'alnum':
+            res = "%sd" % degrees
+            if minutes:
+                res += "%02d" % minutes
+            res += hemisphere
+            return res
+
+        if format == 'ascii':
+            res = "%s°" % degrees
+            if minutes:
+                res += "{0:0>2d}'".format(minutes)
+            if seconds:
+                res += '{0:0>2f}"'.format(seconds)
+            res += hemisphere
+            return res
+
+        res = "%s%s" % (degrees, DEGREES)
+
+        if minutes:
+            res += " %s%s" % (minutes, MINUTES)
+
+        if seconds:
+            res += " %s%s" % (seconds, SECONDS)
+        res += " %s" % hemisphere
+        return res
+
+    def lat_to_string(self, format='alnum'):
+        if self.latitude < 0:
+            hemisphere = 'S'
+        else:
+            hemisphere = 'N'
+        degrees, minutes, seconds = dec2degminsec(abs(self.latitude))
+        return self._format(degrees, minutes, seconds, hemisphere, format)
+
+    def lon_to_string(self, format='alnum'):
+        if self.longitude < 0:
+            hemisphere = 'W'
+        else:
+            hemisphere = 'E'
+        degrees, minutes, seconds = dec2degminsec(abs(self.longitude))
+        return self._format(degrees, minutes, seconds, hemisphere, format)
---
+++ tests/test_coordinates.py
@@ -0,0 +1,65 @@
+import pytest
+
+from clldutils.coordinates import *
+
+
+@pytest.mark.parametrize(
+    'dec,hem,res',
+    [
+        (2.4, 'NS', "2°24'N"),
+        (-2.35467, 'EW', '2°21\'17.000000"W'),
+    ]
+)
+def test_degminsec(dec, hem, res):
+    assert degminsec(dec, hem) == res
+
+
+@pytest.mark.parametrize(
+    'format,coord,lat,lon',
+    [
+        ('aln', ('13dN', 0), 13.0, 0),
+        ('degminsec', ('1°1′1″N', '1°1′1.5″W'), 1.1, 1.1),
+        ('degminsec', ('1°1′1″N'.encode('utf8'), '1°1′1.5″W'), 1.1, 1.1),
+    ]
+)
+def test_to_dec(format, coord, lat, lon):
+    c = Coordinates(coord[0], coord[1], format=format)
+    assert pytest.approx(c.latitude, lat)
+    assert pytest.approx(c.longitude, lon)
+
+
+def test_bad_coords():
+    with pytest.raises(ValueError):
+        _ = Coordinates('13dxN', 0)
+
+
+@pytest.mark.parametrize(
+    'coord,lat,lon',
+    [
+        ((0, 0), '0dN', '0dE'),
+        ((2.4, 0), '2d24N', '0dE'),
+        ((12.17, 92.83), '12d10N', '92d49E'),
+        ((-12.17, -92.83), '12d10S', '92d49W'),
+        (('12d30N', '60d30E'), '12d30N', '60d30E'),
+    ]
+)
+def test_to_alnum(coord, lat, lon):
+    c = Coordinates(*coord)
+    assert c.lat_to_string() == lat
+    assert c.lon_to_string() == lon
+
+
+def test_roundtrip():
+    for lat in range(-90, 90):
+        lat += 0.15
+        c = Coordinates(lat, 0.222)
+        c2 = Coordinates(
+            c.lat_to_string('degminsec'), c.lon_to_string('degminsec'), format='degminsec')
+        assert pytest.approx(lat, c2.latitude)
+
+    for lon in range(-180, 180):
+        lon += 0.44443444444
+        c = Coordinates(-0.1111, lon)
+        c2 = Coordinates(
+            c.lat_to_string('degminsec'), c.lon_to_string('degminsec'), format='degminsec')
+        assert pytest.approx(lon, c2.longitude)
