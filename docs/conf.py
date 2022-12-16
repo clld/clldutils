ebcabfb2198282529839030778bd3bda82ccf521
Robert Forkel <xrotwang@googlemail.com>
2022-12-16 15:39:23 +0100

    release 3.16.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Robert Forkel'
 author = 'Robert Forkel'
 
 # The full version, including alpha/beta/rc tags
-release = '3.15.2.dev0'
+release = '3.16.0'
 
 
 # -- General configuration ---------------------------------------------------
